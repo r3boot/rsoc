3cdb54311495cd6b73cde232c85b4947c14d8ce7
Lex van Roon <r3boot@r3blog.nl>
2017-12-11 01:41:36 +0100

    Run uname test locally only

--- lib/transport/ssh/ssh_test.go
+++ lib/transport/ssh/ssh_test.go
@@ -1,6 +1,7 @@
 package ssh
 
 import (
+	"os"
 	"strings"
 	"testing"
 )
@@ -10,26 +11,28 @@ const (
 )
 
 func TestRun(t *testing.T) {
-	args := []string{"localhost", "uname"}
-	stdout, stderr, err := Run(args)
-	if len(stdout) > 0 {
-		if !strings.HasPrefix(stdout, TEST_UNAME) {
-			t.Errorf("ssh.Run: uname called, expected %s, got %s", TEST_UNAME, stdout)
+	if os.Getenv("TRAVIS_BUILD_DIR") != "" {
+		args := []string{"localhost", "uname"}
+		stdout, stderr, err := Run(args)
+		if len(stdout) > 0 {
+			if !strings.HasPrefix(stdout, TEST_UNAME) {
+				t.Errorf("ssh.Run: uname called, expected %s, got %s", TEST_UNAME, stdout)
+			}
+		} else {
+			t.Errorf("ssh.Run: uname called, but got no output")
 		}
-	} else {
-		t.Errorf("ssh.Run: uname called, but got no output")
+
+		if len(stderr) > 0 {
+			t.Errorf("ssh.Run uname called, but got stderr output")
+		}
+
+		if err != nil {
+			t.Errorf("ssh.Run uname called, err != nil: %v", err)
+		}
 	}
 
-	if len(stderr) > 0 {
-		t.Errorf("ssh.Run uname called, but got stderr output")
-	}
-
-	if err != nil {
-		t.Errorf("ssh.Run uname called, err != nil: %v", err)
-	}
-
-	args = []string{"--nonexistent", "--commandline", "--parameters"}
-	stdout, stderr, err = Run(args)
+	args := []string{"--nonexistent", "--commandline", "--parameters"}
+	stdout, stderr, err := Run(args)
 	if len(stdout) > 0 {
 		t.Errorf("ssh.Run: invalid ssh params, but got stdout output")
 	}
